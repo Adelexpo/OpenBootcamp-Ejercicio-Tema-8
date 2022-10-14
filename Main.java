0c3305340f78214806869bca389617f55e8a2e28
AdilDev <adilexpo2@gmail.com>
2022-10-14 19:30:15 +0200

    Ejercicio temas la encapsulación

--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        Persona nombre = new Persona();
+        nombre.setTipo("Nombre: Adil");
+
         Persona edad = new Persona();
         edad.setTipo( "Edad: 40");
-
-        Persona nombre = new Persona();
-        nombre.setTipo("Nombre: Adil Eljihad");
 
         Persona telefono= new Persona();
         telefono.setTipo("Teléfono: 123456789");
 
-        System.out.println(edad.getTipo());
         System.out.println(nombre.getTipo());
+        System.out.println(edad.getTipo());
         System.out.println(telefono.getTipo());
     }
 }
